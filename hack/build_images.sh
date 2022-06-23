9342e21cf936513e99aca3f27142df87df8db6c9
Evan Hazlett <ejhazlett@gmail.com>
2022-06-22 23:49:34 -0400

    update image build script

--- hack/build_images.sh
+++ hack/build_images.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 VERSION=${VERSION:-"dev"}
-BUILD=${BUILD:-"-d"}
+BUILD=${BUILD:-""}
 REGISTRY=${REGISTRY:-"docker.io/ehazlett"}
 TAG=${TAG:-"dev"}
 DAEMON=flow
-WORKER=flow-worker
 WORKFLOWS=$(ls cmd/ | grep flow-workflow)
-IMAGE_BUILD_EXTRA=""
+IMAGE_BUILD_EXTRA="${IMAGE_BUILD_EXTRA:-""}"
 PUSH=${PUSH:-""}
 
 if [ ! -z "${PUSH}" ] && [ "${PUSH}" != "n" ] && [ "${PUSH}" != "N" ]; then
     PUSH="--push"
 fi
 
-echo " -> building $DAEMON"
-docker buildx build --build-arg VERSION=$VERSION --build-arg BUILD=$BUILD ${IMAGE_BUILD_EXTRA} -t ${REGISTRY}/${DAEMON}:${TAG} ${PUSH} -f Dockerfile .
+echo " -> building ${VERSION}${BUILD}"
 
-echo " -> building $WORKER"
-docker buildx build --build-arg VERSION=$VERSION --build-arg BUILD=$BUILD ${IMAGE_BUILD_EXTRA} -t ${REGISTRY}/${WORKER}:${TAG} ${PUSH} -f Dockerfile.worker .
+echo " -> building $DAEMON"
+docker buildx build --build-arg VERSION=${VERSION} --build-arg BUILD=${BUILD} ${IMAGE_BUILD_EXTRA} -t ${REGISTRY}/${DAEMON}:${TAG} ${PUSH} -f Dockerfile .
 
 for workflow in $WORKFLOWS; do
     echo " -> building $workflow"
-    docker buildx build --build-arg VERSION=$VERSION --build-arg BUILD=$BUILD --build-arg WORKFLOW="${workflow}" ${IMAGE_BUILD_EXTRA} -t ${REGISTRY}/${workflow}:${TAG} ${PUSH} -f cmd/$workflow/Dockerfile .
+    docker buildx build --build-arg VERSION=${VERSION} --build-arg BUILD=${BUILD} --build-arg WORKFLOW="${workflow}" ${IMAGE_BUILD_EXTRA} -t ${REGISTRY}/${workflow}:${TAG} ${PUSH} -f cmd/$workflow/Dockerfile .
 done
 
