bfdfee57389e07f68f277c239add3b60903c367f
Evan Hazlett <ejhazlett@gmail.com>
2022-07-28 00:58:09 -0400

    update build scripts

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM golang:alpine as build
 RUN apk add -U make git
 ARG VERSION
+ARG COMMIT
 ARG BUILD
 COPY . /src
 WORKDIR /src
-RUN make VERSION=$VERSION BUILD=$BUILD daemon
+RUN make VERSION=$VERSION COMMIT=$COMMIT BUILD=$BUILD daemon
 
 FROM alpine:3.16
 COPY --from=build /src/bin/flow /usr/bin/flow
--- hack/build_images.sh
+++ hack/build_images.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+COMMIT=${COMMIT:-`git rev-parse --short HEAD`}
 VERSION=${VERSION:-"dev"}
 BUILD=${BUILD:-""}
 REGISTRY=${REGISTRY:-"docker.io/ehazlett"}
@@ -20,14 +21,15 @@ fi
 # check for skip
 if [ -z "$SKIP_FLOW" ]; then
     echo " -> building $DAEMON"
-    docker buildx build --build-arg VERSION=${VERSION} --build-arg BUILD=${BUILD} ${IMAGE_BUILD_EXTRA} ${DAEMON_EXTRA} -t ${REGISTRY}/${DAEMON}:${TAG} ${PUSH} -f Dockerfile .
+    docker buildx build --build-arg VERSION=${VERSION} --build-arg COMMIT=${COMMIT} --build-arg BUILD=${BUILD} ${IMAGE_BUILD_EXTRA} ${DAEMON_EXTRA} -t ${REGISTRY}/${DAEMON}:${TAG} ${PUSH} -f Dockerfile .
 fi
 
-for workflow in $WORKFLOWS; do
-    echo " -> building $workflow"
-    if [ ! -z "${UPDATE_LATEST}" ] && [ "${UPDATE_LATEST}" != "n" ] && [ "${UPDATE_LATEST}" != "N" ]; then
-        WORKFLOW_EXTRA="-t ${REGISTRY}/${workflow}:latest"
-    fi
-    docker buildx build --build-arg VERSION=${VERSION} --build-arg BUILD=${BUILD} --build-arg PROCESSOR="${workflow}" ${IMAGE_BUILD_EXTRA} -t ${REGISTRY}/${workflow}:${TAG} ${WORKFLOW_EXTRA} ${PUSH} -f cmd/$workflow/Dockerfile .
-done
-
+if [ -z "$SKIP_WORKFLOWS" ]; then
+    for workflow in $WORKFLOWS; do
+        echo " -> building $workflow"
+        if [ ! -z "${UPDATE_LATEST}" ] && [ "${UPDATE_LATEST}" != "n" ] && [ "${UPDATE_LATEST}" != "N" ]; then
+            WORKFLOW_EXTRA="-t ${REGISTRY}/${workflow}:latest"
+        fi
+        docker buildx build --build-arg VERSION=${VERSION} --build-arg BUILD=${BUILD} --build-arg COMMIT=${COMMIT} --build-arg PROCESSOR="${workflow}" ${IMAGE_BUILD_EXTRA} -t ${REGISTRY}/${workflow}:${TAG} ${WORKFLOW_EXTRA} ${PUSH} -f cmd/$workflow/Dockerfile .
+    done
+fi
